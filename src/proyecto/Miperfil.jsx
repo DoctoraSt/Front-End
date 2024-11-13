2a0217ad8a465cf9f91f6ac9c6f69048c42c43c6
DoctoraSt <leal.lf._@hotmail.com>
2024-11-12 19:06:02 -0600

    form

--- src/proyecto/Miperfil.jsx
+++ src/proyecto/Miperfil.jsx
@@ -13,14 +13,17 @@ function Miperfil(){
             <div className="row">
                 <h2 className="col-4">Información de usuario</h2>
                 <div className="col-6">
+                    <FloatingLabel controlId="floatingLabel" label="Nombre de usuario" className="mb-3">
+                        <Form.Control type="text" placeholder="Nombre de ususario"/>
+                    </FloatingLabel>
                     <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
                             <Form.Control type="email" placeholder="name@example.com" />
                     </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="Password">
+                    <FloatingLabel controlId="floatingPassword" label="Password">
                         <Form.Control type="password" placeholder="Password" />
                     </FloatingLabel>
                     <br />
-                <Button className="coustome-link">Aceptar cambios</Button>
+                <Button  className="coustome-link">Aceptar cambios</Button>
                 <br/>
                 <br/>
                 <Button href={"/lista-compras"} className="coustome-link">Cancelar</Button>
