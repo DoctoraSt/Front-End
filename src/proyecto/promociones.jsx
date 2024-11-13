33b0b424b314a3ce6eaa75978b16236f72171d77
DoctoraSt <leal.lf._@hotmail.com>
2024-11-12 23:44:39 -0600

    imagenes promocionales

--- src/proyecto/promociones.jsx
+++ src/proyecto/promociones.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Promo from '../CSS/images/promos.png';
-import Switch from '../CSS/images/switcho.png';
+import Image from 'react-bootstrap/Image';
+
+// Importa las imágenes directamente
+import promos from '../CSS/images/promos.png';
+import switcho from '../CSS/images/switcho.png';
 import laptop from '../CSS/images/laptop2.png';
 
-function promociones() {
+function Promociones() {
     return (
-    <Carousel>
-        <Carousel.Item>
-            <img src={Promo} alt="Promociones"/>
-        <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img src={Switch} alt="Promoción celulares"/>
-        <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <img src={laptop} alt="Promoción computadores"/>
-        <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-        </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
+        <Carousel>
+            <Carousel.Item>
+                <Image src={promos} alt="Promociones" fluid />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image src={switcho} alt="Promoción celulares" fluid />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image src={laptop} alt="Promoción computadores" fluid />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 }
 
-export default promociones;
+export default Promociones;
